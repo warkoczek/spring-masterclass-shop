4ac3595bc6e91c68b215ea3efbc456abd0c60251
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-03-28 22:38:45 +0200

    Hikari

--- src/main/java/pl/training/shop/ShopConfiguration.java
+++ src/main/java/pl/training/shop/ShopConfiguration.java
@@ -1,12 +1,17 @@
 package pl.training.shop;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
 
+import javax.sql.DataSource;
 
+@PropertySource("classpath:jdbc.properties")
 @EnableAspectJAutoProxy
 @Configuration
 public class ShopConfiguration {
@@ -17,5 +22,14 @@ public class ShopConfiguration {
         messageSource.setBasename("messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+    @Bean
+    public DataSource dataSource(Environment environment){
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setUsername(environment.getProperty("database.username"));
+        dataSource.setPassword(environment.getProperty("database.password"));
+        dataSource.setJdbcUrl(environment.getProperty("database.url"));
+        dataSource.setDriverClassName(environment.getProperty("database.driver"));
+        return dataSource;
     }
 }
