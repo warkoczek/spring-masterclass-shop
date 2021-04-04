765d28b6ca22f2d7df0f877c6b6e324ce340d41b
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-04-04 12:36:48 +0200

    OrderConfiguration

--- src/main/java/pl/training/shop/orders/OrderConfiguration.java
+++ src/main/java/pl/training/shop/orders/OrderConfiguration.java
@@ -1,11 +1,23 @@
 package pl.training.shop.orders;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy
 @Configuration
 public class OrderConfiguration {
+
+    @Bean
+    public OrderRepository orderRepository(){
+        return new HashMapOrderRepository();
+    }
+
+    @Bean
+    public OrderService orderService(OrderRepository orderRepository){
+        return new OrderService(orderRepository);
+    }
+
 
 
 }
