c076623904d520540df3f93b9531ceab230ef658
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-04-15 11:57:01 +0200

    gitignore

--- target/generated-sources/annotations/pl/training/shop/products/ProductMapperImpl.java
+++ target/generated-sources/annotations/pl/training/shop/products/ProductMapperImpl.java
@@ -11,7 +11,7 @@ import pl.training.shop.common.web.PagedResultTransferObject;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-04-13T18:09:25+0200",
+    date = "2021-04-15T11:56:09+0200",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
 )
 @Component
